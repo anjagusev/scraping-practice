4b77805a7db6d7941a1bed36424ce8acbb3f7736
anjagusev <anja.gusev@gmail.com>
2019-04-02 18:59:00 -0400

    able to scrape categories and dish names

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-import { getHTML, getMenuItem } from "./lib/scraper";
+import {
+  getHTML,
+  getMenuItemFromPigCameHome,
+  getItemsFromFoodora
+} from "./lib/scraper";
+
+const pigFoodora =
+  "https://www.foodora.ca/restaurant/s1jc/when-the-pig-came-home";
+
+const pig = "https://www.whenthepigcamehome.ca/menu";
 
 async function go() {
-  const html = await getHTML("https://www.whenthepigcamehome.ca/menu");
-  const menuItems = await getMenuItem(html);
+  // const html = await getHTML("https://www.whenthepigcamehome.ca/menu");
+  // const menuItems = await getMenuItemFromPigCameHome(html);
+  // console.log(menuItems);
+  const html = await getHTML(pigFoodora);
+  const menuItems = await getItemsFromFoodora(html);
   console.log(menuItems);
 }
 
